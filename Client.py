52f288382c5195257dab9272acb8f81b9139e321
Tudor-Gabriel Tomescu <46830306+Herastis@users.noreply.github.com>
2021-03-01 23:24:50 +0200

    2/3 of step 3

--- Client.py
+++ Client.py
@@ -1,24 +1,18 @@
 import socket
 import time
-from Crypto.PublicKey import RSA
-from Crypto.Cipher import PKCS1_OAEP
-from Crypto.Cipher import AES
-from Crypto.Random import get_random_bytes
-from Crypto.Hash import SHA256
-from Crypto.Signature import pss
+from Cryptodome.PublicKey import RSA
+from Cryptodome.Cipher import PKCS1_OAEP
+from Cryptodome.Cipher import AES
+from Cryptodome.Random import get_random_bytes
+from Cryptodome.Hash import SHA256
+from Cryptodome.Signature import pss
 
 host = "localhost"
 port = 9009
 
 #----Optrional
 #Customer
-def key_generator():
-    # return Random.get_random_bytes(16)
-    new_key = RSA.generate(1024)
-    public_key = new_key.publickey().exportKey()
-    private_key = new_key.exportKey()
-    return public_key, private_key
-print(key_generator())
+
 #---------------------------------------------------
 
 #Card and payment details-------
@@ -35,66 +29,58 @@ iv = b'12345678abcdefgh'
 
 #Cheie AES pentru C si PG
 key_cpg = get_random_bytes(16)
-print("CPG", key_cpg)
+print("CPG: ", key_cpg)
 AES_CPG = AES.new(key_cpg, AES.MODE_EAX)
 file_out = open("AES_CPG.bin", "wb")
 file_out.write(key_cpg)
 file_out.close()
 
 if __name__ == '__main__':
-
+    #1.a) Generam cheile RSA ale clientului
     RSA_Ckey = RSA.generate(1024)
-    f = open('RSA_PubKC.pem', 'wb')
-    f.write(RSA_Ckey.publickey().exportKey('PEM'))
-    f.close()
-    f = open('RSA_PrivKC.pem', 'wb')
-    f.write(RSA_Ckey.exportKey('PEM'))
-    f.close()
-    #public, private = key_generator()
-    sb = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sb.connect((host, port))
 
+    #PUBLIC KEY RSA CLIENT
+    public_key = RSA_Ckey.publickey().exportKey()
+    private_key = RSA_Ckey.exportKey()
+
+    #1.c) Cream cheia AES:
+    key = get_random_bytes(16)
+    AES_key = AES.new(key, AES.MODE_EAX, iv)
+    #1.Criptam cheia publica RSA cu AES
+    public_keyEnc = AES_key.encrypt(public_key)
+    print("Random Key: ", key, end='\n\n')
+
+    c = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    c.connect((host, port))
     try:
-        #Public key primit de la Merchant
-        pub_key = RSA.importKey(sb.recv(1024), passphrase=None)
-        print("Public key:", pub_key)
+        #1)Primim cheia publica RSA a MERCHANTULUI si trimitem cheia publica a clientului:
+        pubKM = RSA.importKey(c.recv(1024), passphrase=None) # M -> C
+        c.send(public_keyEnc) # C -> M
+        print("Public key:", pubKM)
 
-        #Criptam prin AES datele cardului
-        key = get_random_bytes(16)
-        AES_key = AES.new(key, AES.MODE_EAX, iv)
-
-        print("Random Key: ", key, end='\n\n')
-        #Criptam numerele cardului, data de expirare, cvv
-        nameOnCardEnc = AES_key.encrypt(nameOnCard)
-        numereCardEnc = AES_key.encrypt(nrCard)
-        validThruEnc = AES_key.encrypt(validThru)
-        cvvEnc = AES_key.encrypt(cvv)
-        amountEnc = AES_key.encrypt(amount)
+        # #Criptam numerele cardului, data de expirare, cvv
+        # nameOnCardEnc = AES_key.encrypt(nameOnCard)
+        # numereCardEnc = AES_key.encrypt(nrCard)
+        # validThruEnc = AES_key.encrypt(validThru)
+        # cvvEnc = AES_key.encrypt(cvv)
+        # amountEnc = AES_key.encrypt(amount)
 
         # Cheia AES criptata cu cea publica
-        encryptor = PKCS1_OAEP.new(pub_key)
+        #Criptam cheia AES cu cheia publica a lui Merchant
+        encryptor = PKCS1_OAEP.new(pubKM)
         encrAES = encryptor.encrypt(key)
 
-        #Trimitem mesajul criptat la Merchant
-        sb.sendall(encrAES)
-
-        #Trimitem datele cardului criptate
-        sb.send(nameOnCardEnc)
-        time.sleep(0.2)
-        sb.send(numereCardEnc)
-        time.sleep(0.2)
-        sb.send(validThruEnc)
-        time.sleep(0.2)
-        sb.send(cvv)
+        #1.Trimitem cheia AES criptata la Merchant
+        c.sendall(encrAES)
 
         # Recream aceeasi cheie AES pentru criptare
         aes_key = AES.new(key, AES.MODE_EAX, AES_key.nonce)
 
         #Primim de la Merchant Sid si SgM(Sid) criptate
-        SidEnc = sb.recv(1024)
+        SidEnc = c.recv(1024)
         print("Sid encrypted: ", SidEnc)
 
-        signatureEnc = sb.recv(1024)
+        signatureEnc = c.recv(1024)
         print("Signature encrypted:", signatureEnc, end='\n\n')
 
         Sid = aes_key.decrypt(SidEnc)
@@ -106,47 +92,64 @@ if __name__ == '__main__':
         #PI----------------------------------------------------------->
         #PI(concatenarea)
         pi = nameOnCard + validThru + nrCard + Sid + amount + nc + m #+ pb_key
-        print("Date concatenate: ", pi)
+        print("PI: ", pi)
 
         #Aplicam semnatura pe PI
         f = open('RSA_PrivKC.pem', 'r')
         key = RSA.import_key(f.read())
         h = SHA256.new(pi)
-        signaturePI = pss.new(key).sign(h)
-        print("Semnatura datelor concatenate: ", signaturePI, end='\n\n')
+        SigC_PI = pss.new(key).sign(h)
+        print("Signature PI: ", SigC_PI, end='\n\n')
 
         #Criptez pi si signaturePI cu AES
         piEnc = AES_CPG.encrypt(pi)
-        signaturePIEnc = AES_CPG.encrypt(signaturePI)
+        signaturePIEnc = AES_CPG.encrypt(SigC_PI)
         print("PI criptat: ", piEnc)
-        print("Semnatura PI criptat: ", signaturePIEnc)
+        print("Signature PI criptat: ", signaturePIEnc)
         # PI<-----------------------------------------------------------
 
         #PO----------------------------------------------------------->
         #SigC(concatenarea)
-        sigC =  Sid + amount + nc # + OrderDesc +
-        print("SigC: ",sigC)
+        SigC_ord_sid_amound_nc = Sid + amount + nc #+ OrderDesc +
+        print("SigC: ", SigC_ord_sid_amound_nc)
 
         #Aplicam semnatura pe SigC
         f = open('RSA_PrivKC.pem', 'r')
         key = RSA.import_key(f.read())
-        h = SHA256.new(sigC)
+        h = SHA256.new(SigC_ord_sid_amound_nc)
         signatureC = pss.new(key).sign(h)
         print("Signature C: ", signatureC, end='\n\n')
 
         #PO(Concatenam)
-        po = Sid + amount + nc + signatureC # + OrderDesc +
+        po = Sid + amount + nc + signatureC #+ OrderDesc +
         print("PO: ", po)
 
-        # Criptam PO
-        poEncript = AES_key.encrypt(po)
+        # Criptam PO si SigC
+        poEnc = AES_key.encrypt(po)
+        print("PO criptat: ", poEnc, end='\n\n')
+        SigC_ord_sid_amound_nc_ENC = AES_key.encrypt(signatureC)
+        print("Signature C criptat: ", SigC_ord_sid_amound_nc_ENC, end='\n\n')
         # PO<-----------------------------------------------------------
 
-        # TRimitem PM si PO catre M
+        #dic = {Sid, amount, nc, signatureC} #+ OrderDesc +}
+        #print(dic)
 
-        sb.send(poEncript)
-        sb.send(piEnc)
-        sb.send(signaturePIEnc)
+        # Pasul 3:Trimitem PM si PO catre M
+        c.send(poEnc)
+        time.sleep(0.2)
+        c.send(piEnc)
+        time.sleep(0.2)
+        c.send(signaturePIEnc)
+
+        pm = piEnc + SigC_ord_sid_amound_nc_ENC
+        pmEnc = AES_CPG.encrypt(pm)
+
+        #Avem poEnc, pmEnc criptate
+        Pas3 = pmEnc + poEnc
+        print("Pas3:", Pas3)
+
+        #Trimitem catre merchant
+        c.send(Pas3)
 
     finally:
-        sb.close()
+        c.close()
--- Merchant.py
+++ Merchant.py
@@ -1,25 +1,27 @@
 import time
-from Crypto.Cipher import AES, PKCS1_OAEP
-from Crypto.Cipher import AES
-from Crypto.Signature import pss
-from Crypto.Hash import SHA256
-from Crypto.PublicKey import RSA
-from Crypto.Random import get_random_bytes
-from Crypto import Random
+import random
+from Cryptodome.Cipher import AES, PKCS1_OAEP
+from Cryptodome.Cipher import AES
+from Cryptodome.Signature import pss
+from Cryptodome.Hash import SHA256
+from Cryptodome.PublicKey import RSA
+from Cryptodome.Random import get_random_bytes
+from Cryptodome import Random
 import socket
 
 #c = client
-
 host = 'localhost'
 port = 9009
 iv = b'12345678abcdefgh'
 if __name__ == '__main__':
-    RSA_keys = RSA.generate(1024)
-    private_key = RSA_keys.exportKey()
-    f = open('mykey.pem', 'wb')
-    f.write(RSA_keys.export_key('PEM'))
-    f.close()
+    #1.b) Generam cheile RSA ale merchantului
+    RSA_Mkeys = RSA.generate(1024)
+    public_key = RSA_Mkeys.publickey().exportKey()
+    private_key = RSA_Mkeys.exportKey()
 
+    # f = open('mykey.pem', 'wb')
+    # f.write(RSA_Mkeys.export_key('PEM'))
+    # f.close()
 
     print("Private key: ",private_key)
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -27,38 +29,31 @@ if __name__ == '__main__':
     s.listen(1)
     c, addr = s.accept()
     try:
-        #Trimitem cheia public catre client
-        c.send(RSA_keys.publickey().exportKey(format='PEM', passphrase=None, pkcs=1))
+        #1)Trimitem cheia publica RSA catre client si primim cheia publica a clientului:
+        c.send(RSA_Mkeys.publickey().exportKey(format='PEM', passphrase=None, pkcs=1)) # M -> C
+        public_keyEnc = c.recv(1024) # C -> M
 
-        #Primim mesajul criptat (cheia AES criptata)
+        #Primim cheia AES criptata
         encrAES = c.recv(1024)
 
-        #Primim datele cardului
-        nameOnCardEnc = c.recv(1024)
-        numereCardEnc = c.recv(1024)
-        validThruEnc = c.recv(1024)
-        cvvEnc = c.recv(1024)
+        # #Primim datele cardului
+        # nameOnCardEnc = c.recv(1024)
+        # numereCardEnc = c.recv(1024)
+        # validThruEnc = c.recv(1024)
+        # cvvEnc = c.recv(1024)
 
         #Obtinem decr cu care decriptam datele cardului criptate cu AES
-        decryptor = PKCS1_OAEP.new(RSA_keys)
+        decryptor = PKCS1_OAEP.new(RSA_Mkeys)
         decrAES = decryptor.decrypt(encrAES) #sir random caracter din client
         print("Random Key from Client: ", decrAES, end='\n\n')
         AES_key = AES.new(decrAES, AES.MODE_EAX, iv)
 
 
-        #Decriptam datele cardului
-        nameOnCard = AES_key.decrypt(nameOnCardEnc)
-        numereCard = AES_key.decrypt(numereCardEnc)
-        validThru = AES_key.decrypt(validThruEnc)
-        cvv = AES_key.decrypt(cvvEnc)
-
-        print("Nume: ", nameOnCard)
-        print("Numar Card:", numereCard)
-        print("Data expirare:", validThru)
-        print("CVV :", cvvEnc, end='\n\n')
-
         #Semnatura
-        Sid = b'007'
+        #PASUL 2
+
+        Sid = b'1000'
+        #Semnatura pe SID
         f = open('mykey.pem', 'r')
         key = RSA.import_key(f.read())
         h = SHA256.new(Sid)
@@ -68,7 +63,6 @@ if __name__ == '__main__':
 
         #Recream aceeasi cheie AES pentru criptare
         aes_key = AES.new(decrAES, AES.MODE_EAX, AES_key.nonce)
-
 
         #Criptam cu AES Sid si SgM(Sid)
         SidEnc = aes_key.encrypt(Sid)
@@ -82,9 +76,15 @@ if __name__ == '__main__':
         c.send(signatureEnc)
         time.sleep(0.2)
 
-        pm = c.recv(1024)
+        #Primim datele criptate de la pasul 3
+        poEnc = c.recv(1024)
+        print("poEnc :", poEnc)
         piEnc=c.recv(1024)
-        piSgn = c.recv(1024)
+        print("piEnc :", piEnc)
+        signaturePIEnc = c.recv(1024)
+        print("signaturePIEnc :", signaturePIEnc)
+        Pas3 = c.recv(1024)
+        print("Pas3 :", Pas3)
 
     finally:
         c.close()
